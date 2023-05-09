4696e09669020397e27bf08b4002e2c26cfa0bf1
Mitema Emmanuel <mitema.emmanuel@gmail.com>
2023-05-09 16:50:14 +0100

    removed comments from the css and created a form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,107 @@ import { useState } from 'react'
 
 export default function Home() {
 
-  const [value, setValue] = useState<String>("")
-  function handleSubmit()  {
-    console.log("target")
-  }
+  const [value, setValue] = useState("");
+  const [yearEstablished, setYearEstablished] = useState("");
+  const [amountValue, setAmountValue ] = useState("")
+  const [summaryOfProfitValue, setSummaryOfProfitValue] = useState("")
+  const [accountProvider, setAccountProvider] = useState("")
 
-  function handleChange() {
-    console.log("change")
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-     <label>
-          Name
-            <input
-            name="numberOfGuests"
-            type="number"
-            value={""}
-            />
-      </label>
-        <br />
-      <label>
-          Number of guests:
-          <input
-            name="numberOfGuests"
-            type="number"
-            value={""}
-            />
-      </label>
-      </form>
-
-   
+  const [result, setResult] = useState<any>();
   
-   )
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement> ) => {
+      e.preventDefault();
+      const formData = {
+        name: value,
+        establishedYear: yearEstablished,
+        valueAmount: amountValue,
+        summaryOfProfitValue: summaryOfProfitValue,
+        accountProvider: accountProvider
+      }
+
+      console.log(formData)
+     
+      fetch("/api/form", {
+        body: formData.toString(),
+        method: 'post',
+        headers: {
+          "content-type" : "application/x-www-form-urlencoded"
+        },
+      }).then(async (result)=> {
+
+          setResult(formData)
+       
+      })
+    }
+
+  function handleChange(event:any) {
+    console.log(event.target.value)
+    const value =  event.target.value;
+    setValue(value);
+  }
+ 
+  return (
+    <div className={styles.cardContainer}>
+    <div className={styles.card}>
+    <form className={styles.formContainer} onSubmit={handleSubmit}>
+      <div className={styles.nameContainer}>
+        <label className={styles.nameLabel}>Name:
+          <input
+            type="text" 
+            value={value}
+            onChange={handleChange}
+            className ={styles.nameInputBox}
+            name="name"
+          />
+        </label>
+      </div>
+      <div className={styles.yearEstablishedContainer}>
+      <label className={styles.yearEstablishedLabel}>Year Established:
+        <input
+          type="text" 
+          value={yearEstablished}
+          onChange={ (e)=> {setYearEstablished(e.target.value)}}
+          className ={styles.yearEstablishedBox}
+          name="year established"
+        />
+      </label>
+      </div>
+      <div className = {styles.summaryOfProfitLabelContainer}>
+      <label className={styles.summaryOfProfitLabel}>Summary of profit <br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;or loss by year:
+        <input
+          type="text" 
+          value={summaryOfProfitValue}
+          onChange={(e)=> setSummaryOfProfitValue(e.target.value)}
+          className ={styles.summaryOfProfitBox}
+          name="summary of profit"
+        />
+      </label>
+      </div>
+      <div className={styles.amountContainer}>
+      <label className={styles.amountLabel}>Amount:
+        <input
+          type="text" 
+          value={amountValue}
+          onChange={(e)=> {setAmountValue(e.target.value)}}
+          className ={styles.amount}
+          name="amount"
+        />
+      </label>
+      </div>
+      <div>
+      <label  className={styles.labelAccountProvider}>Account Provider:</label>
+          <select className={styles.optionsAccountProvider} name="cars" id="cars" onChange={e=> setAccountProvider(e.target.value)}>  
+            <option value = ""></option>
+            <option value="xerox">Xerox</option>
+            <option value="myob">myob</option>
+          </select>
+      </div>
+      <div className={styles.submitContainer}><input className={styles.submitInputBox} type="submit" value="Submit"/> </div>    
+    </form>
+    <div>{JSON.stringify(result, null, 4)}</div>
+    </div>
+    </div>
+  )
+  
+   
 }
---
+++ src/pages/api/form.ts
@@ -0,0 +1,6 @@
+export default async function handler(req:any,res:any){
+    // body: {
+    //     email
+    // }
+    res.status(200).json({name:'Home API ROUTE'})
+}
